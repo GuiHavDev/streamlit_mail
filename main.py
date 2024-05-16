9e7d90f07f446d6a9af55712c622cfbee041e8b8
GuiHavDev <163348501+GuiHavDev@users.noreply.github.com>
2024-05-16 17:35:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import streamlit as st
 
 st.set_page_config(page_title="Generateur de mail", page_icon=":robot:")
-st.header("My Header")
+st.header("Generateur de mail")
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-  st.write("col1")
+  st.markdown("Régulièrement, les professionnels aimerait améliorer la qualité de leurs mails mais n'ont pas les compétences pour le faire. Cet outil vous aidera à améliorer vos compétences en écriture de mail en augmentant vos mails grâce à l'IA générative.")
 
 with col2:
   st.write("col2")
-
-with col3:
-  st.write("col3")
-  #st.markdown("")
