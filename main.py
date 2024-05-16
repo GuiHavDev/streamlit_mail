0d7d2233dfc0ea486fe6b1ac9392472dbddd6191
GuiHavDev <163348501+GuiHavDev@users.noreply.github.com>
2024-05-16 17:10:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import streamlit as st
 st.set_page_config(page_title="Generateur de mail", page_icon=":robot:")
 st.header("My Header")
 
-col1, col2 = st.colums(2)
+col1, col2, col3 = st.colums(3)
+
+with col1:
+  st.write("col1")
+
+with col2:
+  st.write("col2")
+
+with col3:
+  st.write("col3")
+  #st.markdown("")
